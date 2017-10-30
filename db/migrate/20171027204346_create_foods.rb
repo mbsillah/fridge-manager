0fffde24ee8cb133838022a0a3e01eceea6884c3
sillah2010 <macktastic2014@gmail.com>
2017-10-30 09:02:43 -0400

    Fridge, food, drink class made

---
+++ db/migrate/20171027204346_create_foods.rb
@@ -0,0 +1,11 @@
+class CreateFoods < ActiveRecord::Migration[5.1]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.integer :weight
+      t.boolean :vegan
+
+      t.timestamps
+    end
+  end
+end
